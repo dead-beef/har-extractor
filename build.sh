b040a9c3f57c8b2ba6cf8fa03f354e6e2a6b2308
dead-beef <dead-beef@users.noreply.github.com>
2017-11-13 01:43:18 +0000

    update build script

--- build.sh
+++ build.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
-rm -rf build/* dist/*
-sed -r 's/`([^`]+)`/\1/g' README.md \
-    | pandoc -f markdown_github -t rst >README.rst
-python3 setup.py sdist
-python3 setup.py bdist_wheel
+rm -rfv build/* dist/* \
+    && (sed -r 's/`([^`]+)`/\1/g' README.md \
+        | pandoc -f markdown_github -t rst >README.rst) \
+    && python3 setup.py sdist bdist_wheel
